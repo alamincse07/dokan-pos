a7327adb7c219b72d50a6a62b6242165c6ab37c9
Alamin <alamincse07@gmail.com>
2024-10-21 15:34:45 +0600

    label access to all

--- protected/controllers/LabelsController.php
+++ protected/controllers/LabelsController.php
@@ -12,34 +12,7 @@ class LabelsController extends Controller
 	/**
 	 * @return array action filters
 	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
-
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-    {
-        //die($this->action->id);
-        return array(
-
-            array('allow', // allow admin user to perform 'admin' and 'delete' actions
-                //'actions'=>$this->getAction(),
-                'actions'=>$this->getActions(),
-                'users'=>$this->getAccess(),
-            ),
-            array('deny',  // deny all users
-                'users'=>array('*'),
-            ),
-        );
-    }
+	
 
 	/**
 	 * Displays a particular model.
